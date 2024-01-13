b8826e6721211196da792b2140bbc7dd48b680bf
Leandro Amoras <amorasleandro@gmail.com>
2024-01-13 20:53:56 -0300

    Mentoria dan dan

---
+++ Mentoria/dan dan/python/aula011.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+#print("Olá, seja bem vindo {}{}".format(nome, sobrenome))
+print(f"Olá, seja bem vindo {nome} {sobrenome}")/home/amoras/dan dan
