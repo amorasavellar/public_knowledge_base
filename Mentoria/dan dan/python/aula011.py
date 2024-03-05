cf417e3afce90788eb0abe23291f6c75e91a7309
Leandro Amoras <amorasleandro@gmail.com>
2024-03-04 23:11:22 -0300

    remove dan dan mentoria

--- Mentoria/dan dan/python/aula011.py
+++
@@ -1,5 +0,0 @@
-nome = input("Digite seu nome: ")
-sobrenome = input("Digite seu sobrenome: ")
-
-#print("Olá, seja bem vindo {}{}".format(nome, sobrenome))
-print(f"Olá, seja bem vindo {nome} {sobrenome}")/home/amoras/dan dan
