131c797497bfaba0494face8867f98d421f19ab5
Leandro Amoras <amorasleandro@gmail.com>
2024-03-06 00:04:52 -0300

    add projeto requests

---
+++ Data Engineering/Alura - Trabalhando com APIs/Projects/main.py
@@ -0,0 +1,9 @@
+#%%
+import requests
+import base64
+
+#%%
+client_id = 'a2dfdd633a73420f861576fcbff26dae'
+client_secret = '54313dd6f3a648c299539063481753d8'
+url = 'https://accounts.spotify.com/api/token'
+header = ''
